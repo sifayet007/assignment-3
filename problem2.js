2ab6d9afcbf79b6b0d871b8c59b0967abbe832d6
sifayet007 <sifayet2004@gmail.comm>
2025-03-11 16:56:50 +0600

    added prblems 2

--- problem2.js
+++ problem2.js
@@ -1,11 +1,8 @@
 function sendNotification(email) {
-  if (!gmail.includes("@")) {
-    return "Invalid Input";
+  if (!email.include("@")) {
+    return "Invalid Email";
   }
-  const splitEmail = email.split("@");
-  const joinEmail = splitEmail.join(" sent you an email from ");
-  return joinEmail;
 }
-const gmail = "zihad.ph@gmail.com";
+const gmail = "zihad@gmail.com";
 const result = sendNotification(gmail);
 console.log(result);
