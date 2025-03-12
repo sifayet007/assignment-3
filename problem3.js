f661a006f687f63474088cfbb3b492414fa6c441
sifayet007 <sifayet2004@gmail.comm>
2025-03-12 11:45:04 +0600

    problem 3 solbed

--- problem3.js
+++ problem3.js
@@ -2,7 +2,12 @@ function checkDigitsInName(names) {
   if (typeof names !== "string") {
     return "Invalid Input";
   }
-  for( const name of names)
+  for (const name of names) {
+    if (/\d/.test(name)) {
+      return true;
+    }
+  }
+  return false;
 }
-const digitsName = checkDigitsInName("rafi123");
+const digitsName = checkDigitsInName("sifat123");
 console.log(digitsName);
