8ff1d6224846105f778542f3f9b99f8221a72735
sifayet007 <sifayet2004@gmail.comm>
2025-03-12 11:17:10 +0600

    problem 2 solbed

--- problem2.js
+++ problem2.js
@@ -1,8 +1,12 @@
 function sendNotification(email) {
-  if (!email.include("@")) {
+  if (!email.includes("@")) {
     return "Invalid Email";
   }
+  const emailSplit = email.split("@");
+  const emailJoin = emailSplit.join(" sent you an email from ");
+  return emailJoin;
 }
 const gmail = "zihad@gmail.com";
+
 const result = sendNotification(gmail);
 console.log(result);
---
+++ problem3.js
@@ -0,0 +1,8 @@
+function checkDigitsInName(names) {
+  if (typeof names !== "string") {
+    return "Invalid Input";
+  }
+  for( const name of names)
+}
+const digitsName = checkDigitsInName("rafi123");
+console.log(digitsName);
